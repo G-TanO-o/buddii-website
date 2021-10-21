f971d5cc13db7ee639c4a469b1150ae93577d6f6
conor <conor.vanoystaeyen@hotmail.com>
2021-10-21 12:00:49 +0200

    first commit conor

--- src/components/ServicesConnect/index.jsx
+++ src/components/ServicesConnect/index.jsx
@@ -1,26 +1,29 @@
-import React, {useRef, useEffect, useState} from "react";
+import React, { useRef, useEffect, useState } from "react";
 
+import ServiceCard from "./ServiceCard"
 import make from "../../assets/images/powerpoint/make.svg";
-import Button from "../Button";
+import components from "../../assets/images/powerpoint/components.svg";
+import flexibility from "../../assets/images/powerpoint/flexibility.svg";
+import layout from "../../assets/images/powerpoint/layout.svg";
+import user from "../../assets/images/powerpoint/user.svg";
+import organised from "../../assets/images/powerpoint/organised.svg";
 
 import "./servicesConnect.scss";
 
 function ServicesConnect() {
-  
+
   return (
     <>
       <section className="service">
         <div className="service_box">
           <h1 className="service_title">Services that connect you to your users</h1>
           <div className="service_grid">
-            <div className="grid_item">
-              <img className="service_item_img" src={make} alt="" />
-              <div>
-                <p className="service_item_title">Mobile application</p>
-                <p className="service_item_text">Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat.</p>
-              </div>
-              <Button text="Explore" to="#" style="second"/>
-            </div>
+            <ServiceCard img={make} title="Mobile Application" text="Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat." />
+            <ServiceCard img={user} title="Web Application" text="Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat." />
+            <ServiceCard img={layout} title="User Interface Design" text="Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat." />
+            <ServiceCard img={components} title="Strategy and Branding" text="Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat." />
+            <ServiceCard img={flexibility} title="Performance Marketing" text="Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat." />
+            <ServiceCard img={organised} title="Project Management" text="Lorem ipsum dolor amet, consectetur adipiscing elit. Mattis et sed nam sem tellus erat." />
           </div>
         </div>
       </section>
